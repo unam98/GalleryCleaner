481951dbc2235380249f4c51877792abd4948f71
unam98 <dnska6657@gmail.com>
2026-06-28 17:11:52 +0900

    chore: build 폴더 gitignore 추가

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_unam_photocleaner_PhotoCleanerApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.unam.photocleaner.PhotoCleanerApp"
-)
-public final class _com_unam_photocleaner_PhotoCleanerApp {
-}
